9cd7e2f60d865c36e0f02818a2d9f4fc38530bff
Birk Johannessen <>
2025-01-24 23:07:32 +0100

    format code sanely

---
+++ stow/.config/nvim/lua/birkj/scripts.lua
@@ -0,0 +1,12 @@
+local m = {}
+
+function m.saneformat()
+  vim.opt.expandtab = true
+  vim.opt.tabstop = 4
+  vim.opt.softtabstop = 4
+  vim.opt.shiftwidth = 4
+  -- vim.cmd("%retab")
+  -- vim.cmd("normal! gg=G")
+end
+
+vim.api.nvim_create_user_command("Saneformat", m.saneformat, {})
